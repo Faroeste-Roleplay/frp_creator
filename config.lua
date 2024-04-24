b76fd2abe1c660ddf6af858af04ac5a9fa8b8f12
Abdulkadir AKTAŞ <abdulkadir.aktas@hotmail.com>
2024-04-24 22:34:02 +0300

    fix(missing CutsceneArrivalCoords on config)

--- config.lua
+++ config.lua
@@ -3,3 +3,4 @@ Config = {}
 Config.EnableCutscene = true  --// ENABLE IF YOU CAN WAGON CUTSCENE
 Config.fadeOutTime = 12000 -- // Waiting time to remove fadeout
 Config.FirstSpawnCoords = vector3(2539.83,-1129.33,50.03)  -- // COORDINATES TO SPAWN PLAYER IF CUTSCENE IS DISABLED
+Config.CutsceneArrivalCoords = vector3(2600.436,-1205.932,53.323)
